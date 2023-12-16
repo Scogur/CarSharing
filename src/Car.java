57ac1c908e2a0697de8f4f6a80a19b29d9c8ab0b
Scogur <113353238+Scogur@users.noreply.github.com>
2023-12-16 13:49:03 +0700

    car class minor changes

--- src/Car.java
+++ src/Car.java
@@ -2,10 +2,22 @@ public class Car {
     Integer id;
     String model;
 
+    Float hourCost;
+
     public Car(Integer id, String model) {
         this.id = id;
         this.model = model;
     }
+    public Car(Integer id, String model, Float hourCost){
+        this.id = id;
+        this.model = model;
+        this.hourCost = hourCost;
+    }
+
+    public Float getHourCost(){
+        return hourCost;
+    }
+
 
     public Integer getId() {
         return id;
@@ -17,5 +29,9 @@ public class Car {
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public void setHourCost(Float hourCost){
+        this.hourCost = hourCost;
     }
 }
